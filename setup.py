49611cb3d484f2bd9875c53e009888e8387e5b79
Anish Deepak <59416407+AnishDeepak@users.noreply.github.com>
2023-12-19 17:20:49 +0530

    requirements and half of the setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+with open('README.md','r',encoding='utf-8') as f:
+    long_description=f.read()
+
+__version__='0.0.0'
+REPO_NAME='text_summariser'
+AUTHOR_NAME='Anish Deepak'
+SRC_REPO='text_summariser'
+AUTHOR_EMAIL='anishdeepak34@gmail.com'
