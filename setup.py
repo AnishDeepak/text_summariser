0eda435e75e1c73ca35f8aeaa738fe1bf9992caa
Anish Deepak <59416407+AnishDeepak@users.noreply.github.com>
2023-12-20 15:32:39 +0530

    req and setup comlted

--- setup.py
+++ setup.py
@@ -7,3 +7,13 @@ REPO_NAME='text_summariser'
 AUTHOR_NAME='Anish Deepak'
 SRC_REPO='text_summariser'
 AUTHOR_EMAIL='anishdeepak34@gmail.com'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
+    long_description=long_description,
+    long_description_content='text/markdown',
+    url=f'https://github.com/AnishDeepak/text_summariser'
+)
